b0542d21a9e1ae3b7c6af919cbb92bcc386c459a
Samuu10 <svallmun@myuax.com>
2024-12-09 11:08:43 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microservicioemergencias/MicroservicioEmergenciasApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioemergencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEmergenciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioEmergenciasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microservicioemergencias/MicroservicioEmergenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioemergencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEmergenciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
